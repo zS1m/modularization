a66b53555f4c8c198598e2a96c9411968d121a31
zS1m <917451540@qq.com>
2022-07-27 22:43:29 +0800

    项目创建及eslint prettier配置

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2, // 缩进长度
+  semi: true, // 句末使用分号
+  singleQuote: true, // 使用单引号
+  arrowParens: "always", // 箭头函数小括号
+  trailingComma: 'none', // 关闭尾随逗号
+  endOfLine: 'auto' // 不检查分隔符
+};
---
+++ src/views/module-preview.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>测试用</div>
+</template>
+
+<script>
+export default {
+  name: 'module-preview'
+};
+</script>
+
+<style scoped></style>
