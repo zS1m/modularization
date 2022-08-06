1420055f6aabce0dec455b5cb24254ed07420094
zS1m <917451540@qq.com>
2022-08-06 18:10:27 +0800

    echarts折线图封装

--- src/views/module-preview.vue
+++ src/views/module-preview.vue
@@ -1,11 +1,55 @@
 <template>
-  <div>测试用</div>
+  <div class="module-preview">
+    <div class="chart-block">
+      <chart-line label="连接时延(ms)" :data="testData" :threshold="30"></chart-line>
+    </div>
+  </div>
 </template>
 
 <script>
+import ChartLine from "@/components/chart-line";
+
 export default {
-  name: 'module-preview'
+  name: 'module-preview',
+  components: { ChartLine },
+  data() {
+    return {
+      testData: [
+        {
+          name: '上海',
+          data: [
+            ['2022-08-06 17:20:20', 15],
+            ['2022-08-06 17:30:20', 20],
+            ['2022-08-06 17:40:20', 30],
+            ['2022-08-06 17:50:20', 25],
+            ['2022-08-06 18:00:20', 35],
+            ['2022-08-06 18:10:20', 10],
+          ]
+        },
+        {
+          name: '北京',
+          data: [
+            ['2022-08-06 17:20:20', 25],
+            ['2022-08-06 17:30:20', 10],
+            ['2022-08-06 17:40:20', 20],
+            ['2022-08-06 17:50:20', 15],
+            ['2022-08-06 18:00:20', 30],
+            ['2022-08-06 18:10:20', 35],
+          ]
+        }
+      ]
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.module-preview {
+  height: 100%;
+}
+
+.chart-block {
+  height: 300px;
+  margin-top: 5rem;
+}
+</style>
